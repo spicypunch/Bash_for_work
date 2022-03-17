95f258053c6e6afa71d168d13f174231b596b7a0
spicypunch <72846127+spicypunch@users.noreply.github.com>
2022-03-17 09:42:56 +0900

    Update dns_response_test.sh

--- dns_response_test.sh
+++ dns_response_test.sh
@@ -3,11 +3,16 @@
 n=0
 while [ $n -le 100 ]
 do
+  #명령어 수행 시간 테스트
   StartTime=$(date +%s)
   echo "$n 번 반복"
   nslookup h00027.vegas-solution.com
   EndTime=$(date +%s)
+  
+  #명령어 수행 시간 결과 출력
   echo "It takes $(($EndTime - $StartTime)) seconds to complete this task."
+  
   ((n++))
-  sleep 1s
+  #1초 지나고 다시 반복
+  sleep 0.5s
 done
