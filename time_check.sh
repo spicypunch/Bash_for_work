4a706584333ec6aa8242b11b84929664946e2d12
spicypunch <72846127+spicypunch@users.noreply.github.com>
2022-03-17 10:00:08 +0900

    Update time_check.sh

--- time_check.sh
+++ time_check.sh
@@ -5,14 +5,21 @@ while [ $n -le 100 ]
 do
   #명령어 수행 시간 테스트
   StartTime=$(date +%s)
+  
   echo "$n 번 반복"
   
   #사용할 명령어 기입
   nslookup example.com
+  
   EndTime=$(date +%s)
   
   #명령어 수행 시간 결과 출력
-  echo "It takes $(($EndTime - $StartTime)) seconds to complete this task."
+  echo "명령어를 실행하는데 총 $(($EndTime - $StartTime))초 걸렸습니다."
+  
+  #특정 시간이 지날 때만 확인하고 싶을 때 조건문
+  if [ $(($EndTime - $StartTime)) -ge 5 ]; then
+    echo "명령어를 실행하는데 총 $(($EndTime - $StartTime))초 걸렸습니다."
+  fi
   
   ((n++))
   #0.5초 지나고 다시 반복
