b7a80f40cc1fae05422166f8e57e7bc74e98768f
spicypunch <72846127+spicypunch@users.noreply.github.com>
2022-03-23 11:33:49 +0900

    Update makeDNS.sh

--- makeDNS.sh
+++ makeDNS.sh
@@ -5,5 +5,5 @@ read query
 
 #네임 서버 도메인을 dns1, dns2, dns3을 붙여서 관리하고 있음
 for i in $(seq 1 3); do
-    ssh root@dns$i.domain.com "mysql -uroot -ppasswd -e 'use dpns; $query'"
+    sshpass -f passwd.txt ssh root@dns$i.domain.com "mysql -uroot -ppasswd -e 'use dpns; $query'"
 done
