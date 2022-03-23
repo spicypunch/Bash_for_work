8fc51cf9ebeeb0ef647e35efd536440454924f0c
spicypunch <72846127+spicypunch@users.noreply.github.com>
2022-03-23 11:35:31 +0900

    Update make_new_DB.sh

--- make_new_DB.sh
+++ make_new_DB.sh
@@ -11,17 +11,18 @@ read db_name
 echo -n "DB를 만들 서버의 번호를 입력하시오 [ex)hc4 -> 4] : "
 read server_name
 
+#배열은 0부터 시작하기 때문에 입력받은 server_name의 값을 -1 해준다.
 server_name=${arr[$server_name-1]}
 echo "$server_name"
 
 #공DB를 저장하고 있는 서버에 접속하여 DB를 Dump함
-ssh -pnumber ID@Domain "mysqldump -uroot -ppasswd db > db.sql"
+sshpass -f passwd.txt ssh -pnumber ID@Domain "mysqldump -uroot -ppasswd db > db.sql"
 
 #새로 DB를 생성할 서버에 db.sql을 옮김
-ssh ID2@$server_name "scp -Pnumber ID@Domain:/home/t00000.sql /home/ID2/"
+sshpass -f passwd.txt ssh ID2@$server_name "sshpass -p passwd scp -Pnumber ID@Domain:/home/t00000.sql /home/ID2/"
 
 #입력받은 값을 이름으로 DB 생성
-ssh ID2@$server_name "mysql -uroot -ppasswd -e 'create database $db_name'"
+sshpass -f passwd.txt ssh ID2@$server_name "mysql -uroot -ppasswd -e 'create database $db_name'"
 
 #옮겨놓은 공DB Restore
-ssh ID2@$server_name "mysql -uroot -ppasswd $db_name < db.sql"
+sshpass -f passwd.txt ssh ID2@$server_name "mysql -uroot -ppasswd $db_name < db.sql"
